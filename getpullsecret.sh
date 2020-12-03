094f2d90e8349b3b35d6d0f8039f0ed509ed6957
gwest <gwest@redhat.com>
2020-12-03 17:48:58 -0600

    wip

--- getpullsecret.sh
+++ getpullsecret.sh
@@ -1,4 +1,5 @@
-cp ../gw.lo/install-config.yaml tmp/
+cp gw.lo/install-config.yaml tmp/
 value=$(yq r tmp/install-config.yaml pullSecret)
-echo ${value}
-
+echo ${value} > tmp/pull-secret.json
+export REGUSERAUTH=`echo -n 'dummy:dummy' | base64 -w0`
+cat tmp/pull-secret.json | jq --arg "localhost": { "auth" "xyzz", "email": "dummy@dummy.com" } 
